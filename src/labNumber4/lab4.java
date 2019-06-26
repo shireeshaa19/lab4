ab400a5122286e2b2e1a59e5ad9bde02c78cdc7a
shireeshaa19 <amancha.shireesha1@gmail.com>
2019-06-26 00:39:49 -0400

    second commit

--- src/labNumber4/lab4.java
+++ src/labNumber4/lab4.java
@@ -1,10 +1,51 @@
 package labNumber4;
 
+import java.util.Scanner;
+
 public class lab4 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner scnr = new Scanner(System.in);
+		System.out.println("Learn your squares and cubes!");
+		String ans = "y";
+		do{
+			System.out.println("Enter an integer : " );
+			int integer = scnr.nextInt();
+			
+			System.out.println("Number" + " " + "Squared" + " " + "Cubed");
+			System.out.println("======" + " " + "=======" + " " + "=====");
+			for (int i = 1; i <= integer ; i++){
+				int squared =(int)  Math.pow(i, 2);
+				int cubed = (int) Math.pow(i, 3);
+				
+				System.out.println( i + "      " + squared + "      " + cubed);
+			
+			/*for (int j = 1; j <= integer ; j++){
+				System.out.println((j) + " |");
+				for (int y = 1 ; y <= integer ; y++){
+					System.out.print((y*j) + " ");*/
+					
+					
+					
+				}
+			System.out.println("Would you like to Continue(y/n)");
+			ans = scnr.next();
+		
+     } while (ans.equals("y"));
+		scnr.close();
+    }
+  }
+		
 
-	}
 
-}
+
+		 
+
+	
+		
+		
+
+
+	
+
+	
